2e40b22e71f8ba278398938911afc85f828fb108
DS4506 <davidsmith4506@gmail.com>
2025-09-15 13:53:35 -0500

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Willie Earl on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
